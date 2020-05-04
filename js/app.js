3b5422a7adb22c724ca1cb30eb42082bcdd68533
ahsadek93 <62098165+ahsadek93@users.noreply.github.com>
2020-05-04 10:47:16 +0200

    added Navbar

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var p = new Vue({
+    el: "#topNav",
+    data() {
+        return {
+            windowTop: 0,
+            isScrolled: false
+        };
+    },
+    mounted() {
+        var that = this;
+        window.addEventListener("scroll", function () {
+            that.windowTop = window.scrollY;
+        });
+    },
+
+    watch: {
+        windowTop() {
+            if (this.windowTop != 0) {
+                this.isScrolled = true
+            } else {
+                this.isScrolled = false
+            }
+        }
+    }
+
+});
