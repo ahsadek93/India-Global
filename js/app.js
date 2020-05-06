cb26fe429458f19cbb407654edec3407ce4e8e2d
ahsadek93 <62098165+ahsadek93@users.noreply.github.com>
2020-05-06 12:27:13 +0200

    feedback adjustment 1

--- js/app.js
+++ js/app.js
@@ -72,84 +72,98 @@ var m = new Vue({
                     name: 'Derick Jose & Krishnan Raman',
                     url: './assets/photos/01.jpg',
                     title: 'Co-founders of industrial AI startup Flutura',
-                    text: 'Business drivers of Industry 4.0 in a post-Covid world '
+                    text: 'Business drivers of Industry 4.0 in a post-Covid world ',
+                    soloPic: false
                 },
                 {
                     id: 'm02',
                     name: 'Chintan ',
                     url: './assets/photos/02.jpg',
                     title: 'CXOBridge',
-                    text: 'Strategies for cross-border startups in the new normal'
+                    text: 'Strategies for cross-border startups in the new normal',
+                    soloPic: true
+
                 },
                 {
                     id: 'm03',
                     name: 'Arvind Lakshmikumar',
                     url: './assets/photos/03.jpg',
                     title: 'Co-founder of computer vision startup Tonbo',
-                    text: 'Opportunities for Deep Tech in Defence '
+                    text: 'Opportunities for Deep Tech in Defence ',
+                    soloPic: true
                 },
                 {
                     id: 'm04',
                     name: 'Balaji Viswanathan',
                     url: './assets/photos/04.jpg',
                     title: 'Co-founder of Invento Robotics, maker of Mitra humanoids',
-                    text: 'Robotics in era of social distancing'
+                    text: 'Robotics in era of social distancing',
+                    soloPic: true
+
                 },
                 {
                     id: 'm05',
                     name: 'Ben Matthias and Naganand Doraswamy',
                     url: './assets/photos/05.jpg',
                     title: 'Managing partner of Vertex Ventures - Founder, Ideaspring Capital',
-                    text: 'Investors’ outlook on the future of deep tech from India '
+                    text: 'Investors’ outlook on the future of deep tech from India ',
+                    soloPic: false
                 },
                 {
                     id: 'm06',
                     name: 'Aruna Schwarz',
                     url: './assets/photos/06.jpg',
                     title: 'Co-founder of Stelae Technologies',
-                    text: 'Go to market strategies for Indian deep tech  '
+                    text: 'Go to market strategies for Indian deep tech  ',
+                    soloPic: true
                 },
                 {
                     id: 'm07',
                     name: 'Martin Olausson ',
                     url: './assets/photos/07.jpg',
                     title: 'ABB VP for global startup business development',
-                    text: 'Global enterprise view on Indian deep tech startups'
+                    text: 'Global enterprise view on Indian deep tech startups',
+                    soloPic: true
                 },
                 {
                     id: 'm08',
                     name: 'Madhurima Agarwal ',
                     url: './assets/photos/08.jpg',
                     title: 'Head of Netapp accelerator',
-                    text: 'Corporate accelerator role in identifying opportunities from cloud migration'
+                    text: 'Corporate accelerator role in identifying opportunities from cloud migration',
+                    soloPic: true
                 },
                 {
                     id: 'm09',
                     name: 'Shridhar Marri , Satish Medapati , and Raghu Ravinutala',
                     url: './assets/photos/09.jpg',
                     title: 'Co-founder, Senseforth - Co-founder, Intentico - CEO and co-founder of Yellow Messenger',
-                    text: 'The rise of conversational AI in the wake of social distancing'
+                    text: 'The rise of conversational AI in the wake of social distancing',
+                    soloPic: false
                 },
                 {
                     id: 'm10',
                     name: 'Harshwardhan Kumar and Josef Seuss ',
                     url: './assets/photos/10.jpg',
                     title: 'Co-founders of BlinkIn',
-                    text: 'How remote support helped in Wuhan and new use cases going forward'
+                    text: 'How remote support helped in Wuhan and new use cases going forward',
+                    soloPic: true
                 },
                 {
                     id: 'm11',
                     name: 'Saurabh Chandra',
                     url: './assets/photos/11.jpg',
                     title: 'Co-founder, Ati Motors',
-                    text: 'Future possibilities of autonomous vehicles'
+                    text: 'Future possibilities of autonomous vehicles',
+                    soloPic: true
                 },
                 {
                     id: 'm12',
                     name: 'Lev Mass',
                     url: './assets/photos/12.jpg',
                     title: 'California-based serial entrepreneur, investor and mentor',
-                    text: 'Silicon Valley perspective on opportunities for Indian deep tech startups '
+                    text: 'Silicon Valley perspective on opportunities for Indian deep tech startups ',
+                    soloPic: true
                 },
             ]
         }
